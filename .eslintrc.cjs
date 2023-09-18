3fb872883b10b990a47f7d92e75135423c3d8d66
gaoxianguang <936806643@qq.com>
2023-09-18 11:13:10 +0800

    build: 初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+    "env": {
+        "browser": true,
+        // "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential",
+        // '@vue/eslint-config-typescript',
+        // '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import visualizer from 'rollup-plugin-visualizer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx({
+      // options 参数将传给 @vue/babel-plugin-jsx
+    }),
+    visualizer(),
+  ],
+  
+})
